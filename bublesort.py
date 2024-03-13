870ac5e9768264208023feb571f3f00e19bec31a
affanzaidi <affan.zaidi@vithamastech.com>
2024-03-13 15:57:09 +0530

    new branch with readme file

---
+++ bublesort.py
@@ -0,0 +1,12 @@
+# list = [1,4,2,5,4,6,7,23,4,54,2,34,8,11]
+
+list = [7, 12, 9, 11, 3]
+sortec = []
+for i in list:
+    print(i)
+    print(i)
+    if i > i+1:
+        sortec.append(i)
+        
+        
+print(sortec)
